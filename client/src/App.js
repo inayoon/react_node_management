a322ef5a61a526804441e2af5b161d1ddd0970e9
Ina Yoon <yia0319@gmail.com>
2023-11-15 15:54:02 -0500

    Setting up the project

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
