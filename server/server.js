60526700b31028b2f99229e76dc5d77e995eba87
Ina Yoon <yia0319@gmail.com>
2023-11-15 17:27:56 -0500

    Get data from sql to server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,37 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
+const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const db = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+db.connect();
+
+app.get("/employees", (req, res) => {
+  const sqlGet = "SELECT * FROM EMPLOYEE";
+  db.query(sqlGet, (err, result) => {
+    if (err) return res.json(err);
+    return res.json(result);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.json("Hello world, this is backend server");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running...");
+});
