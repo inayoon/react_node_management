c6e0b8abecd0c967507adf79810b2017256b1faf
Ina Yoon <yia0319@gmail.com>
2023-11-16 17:07:05 -0500

    Create pagination

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./Home.css";
 import { toast } from "react-toastify";
 import axios from "axios";
 
 export default function Home() {
   const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordsPerPage = 10;
+  const lastIndex = currentPage * recordsPerPage;
+  const firstIndex = lastIndex - recordsPerPage;
+  const records = data.slice(firstIndex, lastIndex);
+  const npage = Math.ceil(data.length / recordsPerPage);
+  const numbers = [...Array(npage + 1).keys()].slice(1);
+
   const loadData = async () => {
     const response = await axios.get("http://localhost:5000/employees");
     setData(response.data);
@@ -26,20 +35,24 @@ export default function Home() {
             <th style={{ textAlign: "center" }}>No.</th>
             <th style={{ textAlign: "center" }}>profile</th>
             <th style={{ textAlign: "center" }}>Name</th>
+            <th style={{ textAlign: "center" }}>City</th>
+            <th style={{ textAlign: "center" }}>Branch</th>
             <th style={{ textAlign: "center" }}>Contact</th>
             <th style={{ textAlign: "center" }}>Action</th>
           </tr>
         </thead>
         <tbody>
-          {data.map((item, index) => {
+          {records.map((item, index) => {
+            const uniqueIndex = firstIndex + index + 1;
             return (
               <tr key={item.id}>
-                <th scope="row">{index + 1}</th>
+                <th scope="row">{uniqueIndex}</th>
                 <td>
                   <img src={item.image} alt="avatar" className="avatar" />
                 </td>
                 <td>{item.name}</td>
-
+                <td>{item.city}</td>
+                <td>{item.branch}</td>
                 <td>{item.phone}</td>
                 <td>
                   <Link to={`/update/${item.id}`}>
@@ -61,6 +74,44 @@ export default function Home() {
           })}
         </tbody>
       </table>
+      <nav>
+        <ul className="pagination footer">
+          <li className="page-item">
+            <a href="#" className="page-link" onClick={prePage}>
+              Prev
+            </a>
+          </li>
+          {numbers.map((n, i) => (
+            <li
+              className={`page-item ${currentPage === n ? "active" : ""}`}
+              key={i}
+            >
+              <a href="#" className="page-link" onClick={() => changeCPage(n)}>
+                {n}
+              </a>
+            </li>
+          ))}
+          <li className="page-item">
+            <a href="#" className="page-link" onClick={nextPage}>
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
+
+  function prePage() {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+  function changeCPage(id) {
+    setCurrentPage(id);
+  }
+  function nextPage() {
+    if (currentPage !== npage) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
 }
